496c612e18a1235c695404bf6d5fd0f0183b6a6a
akira <akiraeiphoto@gmail.com>
2018-05-26 02:17:47 +0900

    producing. notihn special

--- src/index.js
+++ src/index.js
@@ -1,18 +1,176 @@
+import './index.scss';
+import axios from 'axios';
 
 
 
-const anchors = {
+//--------------------------variable--------------------------
+//--------------------------variable--------------------------
+//--------------------------variable--------------------------
 
 
 
-}
+const postAPI = axios.create({
+  baseURL: process.env.API_URL
+})
 
 const templates = {
-
-
-  
+  anchor : document.querySelector('.anchor'),
+  login : document.querySelector('#login').content,
+  header : document.querySelector('#header'),
+  newCardbox : document.querySelector('#newCardbox-modal').content,
+  content : document.querySelector('#content').content,
+  cardbox : document.querySelector('#cardbox').content,
+  taskIndex : document.querySelector('#task-index').content,
+  project : document.querySelector('#project').content,
+  taskProject : document.querySelector('#task-project').content,
+  comment : document.querySelector('#comment').content,
+  edit : document.querySelector('#edit-modal').content,
+  footer : document.querySelector('#footer').content
 }
 
+//-----------------------function----------------------------------
+//-----------------------function----------------------------------
+//-----------------------function----------------------------------
+
+function sortDate (arr) {
+  let value =  arr.sort( function (a,b) {
+    let k = b.update - a.update
+    return k
+  })
+  return value
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function render(anchor, fragment) {
+  anchor.textContent = ""; 
+  anchor.appendChild(fragment);
+  console.log("render")
+}
+
+
+
+//----------------template section-----------------------------
+//----------------template section-----------------------------
+//----------------template section-----------------------------
+
+
+
+
+
+//------------------- login  page--------------------------
+//------------------- login  page--------------------------
+
+
+async function loginPage() {
+
+  const fragment = document.importNode(templates.login, true)
+  const form = fragment.querySelector('.login__form')
+  form.addEventListener("submit", async e => {
+    const payload = {
+      username : e.target.elements.id.value,
+      password: e.target.elements.pw.value
+    }
+
+    e.preventDefault()
+
+    const res = await postAPI.post('./users/login', payload)
+
+    e.target.elements.pw.value = ""
+  })
+
+  render(templates.anchor, fragment)
+}
+
+
+//------------------- index  page--------------------------
+//------------------- index  page--------------------------
+
+async function indexPage() {
+
+  const headerFrag = document.importNode(templates.header, true)
+
+  const logoutBtn = headerFrag.querySelector(".header__logout-btn")
+
+  // logoutBtn.addEventListener("click", e => {
+  
+  // })
+  
+
+  const contentFrag = document.importNode(templates.content, true)
+
+  const contentBodyAnchor = contentFrag.querySelector(".content__body-anchor")
+
+  const resCardbox = await postAPI.get('./projects')
+
+  console.log("log1", sortDate(resCardbox.data))
+
+
+  sortDate(resCardbox.data).forEach( async cardbox => {
+    
+    const cardboxId = cardbox.id
+
+    const cardboxFrag = document.importNode(templates.cardbox, true)
+
+    const cardboxAnchor = cardboxFrag.querySelector(".cardbox__anchor")
+
+    cardboxFrag.querySelector(".cardbox__title").textContent = cardbox.title 
+    cardboxFrag.querySelector(".cardbox__update").textContent = cardbox.update
+
+
+
+    const taskIndexFrag = document.importNode(templates.taskIndex, true)
+
+
+    const resTaskIndex = await postAPI.get('./tasks')
+    sortDate(resTaskIndex.data).forEach( async taskIndex => {
+      if(taskIndex.projectId === cardbox.id) {
+        taskIndexFrag.querySelector(".task-index__title").textContent = taskIndex.title
+        cardboxAnchor.appendChild(taskIndexFrag)
+      }
+    })
+ 
+    contentBodyAnchor.appendChild(headerFrag)
+   
+  })
+  render(templates.anchor, headerFrag)
+  render(templates.anchor, contentFrag)
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------------action----------------------
+//--------------------action----------------------
+//--------------------action----------------------
+
+
+// loginPage()
+indexPage()
 
 
 
